d75278e2c2ed0bf060b3e574ab30a909c5287a85
Ernest <ernest.aroozoo@mail.utoronto.ca>
2021-11-11 21:31:34 -0500

    Changed Database into Abstract Class and defined methods to be overridden by Child Class.

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,9 +1,47 @@
 package database;
 
-interface Database {
+public abstract class Database {
+    /**
+     * Abstract class for Database to define the basic layout of methods
+     */
 
-    public String retrieveData(int row, int column); // Row = individual object, Column = information within object
+    public static String readData(int row, int column) {
+        /**
+         * Method to read data from database based on the row and column.
+         * Index for row and column starts at 0.
+         * Format: Name, Address, Sex, Age, Health, Insurance, Symptom A, Symptom B, Symptom C
+         *    0       1      2
+         * 1 Name  Address  Age
+         * 2 Name  Address  Age <-- Reading information given a specific row and column
+         * @return Data contained within the specified row (object) and column (different information within the object)
+         */
+        return null;
+    }
 
-    public boolean lastItem(); // Return true if accessing last row (aka last object in the database)
+    public static void writeData(String name, String address, String sex, int age, int health, boolean insurance,
+                                   String symptomA, String symptomB, String symptomC) {
+        /**
+         * Method to write additional data into a new row of the database.
+         * Index for row and column starts at 0.
+         *    0       1      2
+         * 1 Name  Address  Age
+         * 2 Name  Address  Age
+         * 3 Name  Address  Age <-- New row created to write new information
+         * @return void
+         */
+    }
+
+    public static boolean rowExist(int row) {
+        /**
+         * Index for row and column starts at 0. This method is used to check if this is this row exists
+         *    0       1      2
+         * 1 Name  Address  Age
+         * 2 Name  Address  Age
+         * 3 Name  Address  Age <-- New row created to write new information
+         * 4                    <-- Accessing an invalid row, return False
+         * @return False if the row accessed does not exist or does not contain any data
+         */
+        return true;
+    }
 
 }
