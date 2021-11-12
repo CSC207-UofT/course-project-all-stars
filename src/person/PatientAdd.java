5207202ba426250662a86e50098eae6cf4433b3f
Ernest <ernest.aroozoo@mail.utoronto.ca>
2021-11-11 21:42:59 -0500

    Created PatientAdd class to add new Patient objects into the database.

---
+++ src/person/PatientAdd.java
@@ -0,0 +1,24 @@
+package person;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import database.*;
+
+public class PatientAdd {
+    /**
+     * Responsible for adding Patient object information into database
+     */
+
+    public static void addPatient(String name, String address, String sex, int age, int health, boolean insurance,
+                                  String symptomA, String symptomB, String symptomC){
+        /**
+         * Method to add new Patient object into database.
+         * @return void
+         */
+
+        Database.writeData(name, address, sex, age, health, insurance, symptomA, symptomB, symptomC);
+
+    }
+
+}
