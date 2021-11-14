fc67931b1bf246c3ea31523549ee13cc1a6026bd
alessandromarmi <alessandro.marmi@mail.utoronto.ca>
2021-11-14 16:50:26 -0500

    Added tests for Doctor, Neurologist, Oncologist, Psychiatrist and Cardiologist. Also added the method "LearnHowToCure" in the doctor class.

--- src/person/Doctor.java
+++ src/person/Doctor.java
@@ -7,15 +7,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
     /*
-        A Doctor will be able to cure patients with diseases with a low Urgency Level.
         Doctors from this class will be doctors that don't have a specific specialization.
-        For this reason
+        For this reason they will not be able to cure all the diseases that a patient can have.
     */
 
 public class Doctor extends Person {
     String specialization;
     ArrayList<String> can_cure = new ArrayList<>(Arrays.asList("Salmonella", "Anaphylaxis",
-            "Alcohol poisoning", "Fibromyalgia", "Shingles", "Chickenpox"));
+            "Alcohol poisoning", "Fibromyalgia", "Shingles", "Chickenpox","Covid", "Seasonal Flu",
+            "Tuberculosis", "Arthritis", "Conjunctivitis", "Mononucleosis", "Strep Throat", "Headaches",
+            "Lyme disease", "Malaria", "Measles", "Polio", "Bronchitis","Tetanus", "Rabies"));
 
 
     public Doctor(String name, String address, String sex, int age) {
@@ -35,6 +36,12 @@ public class Doctor extends Person {
             return false;
         }
         return false;
+    }
+
+    public void LearnHowToCure(String disease) {
+        if (!can_cure.contains(disease)) {
+            can_cure.add(disease);
+        }
     }
 
 }
