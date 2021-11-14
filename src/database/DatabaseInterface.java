3d8951e99e26d272cefce740a6567b28ec4e2864
Ernest <ernest.aroozoo@mail.utoronto.ca>
2021-11-13 23:16:06 -0500

    Changed to Interface

--- src/database/DatabaseInterface.java
+++ src/database/DatabaseInterface.java
@@ -1,11 +1,11 @@
 package database;
 
-public abstract class Database {
+public interface DatabaseInterface {
     /**
-     * Abstract class for Database to define the basic layout of methods
+     * Interface to define the basic layout of methods for different databases
      */
 
-    public static String readData(int row, int column) {
+    public String readData(int row, int column);
         /**
          * Method to read data from database based on the row and column.
          * Index for row and column starts at 0.
@@ -15,11 +15,9 @@ public abstract class Database {
          * 2 Name  Address  Age <-- Reading information given a specific row and column
          * @return Data contained within the specified row (object) and column (different information within the object)
          */
-        return null;
-    }
 
-    public static void writeData(String name, String address, String sex, int age, int health, boolean insurance,
-                                   String symptomA, String symptomB, String symptomC) {
+    public void writeData(String name, String address, String sex, int age, int health, boolean insurance,
+                                   String symptomA, String symptomB, String symptomC);
         /**
          * Method to write additional data into a new row of the database.
          * Index for row and column starts at 0.
@@ -29,9 +27,8 @@ public abstract class Database {
          * 3 Name  Address  Age <-- New row created to write new information
          * @return void
          */
-    }
 
-    public static boolean rowExist(int row) {
+    public boolean rowExist(int row);
         /**
          * Index for row and column starts at 0. This method is used to check if this is this row exists
          *    0       1      2
@@ -41,7 +38,5 @@ public abstract class Database {
          * 4                    <-- Accessing an invalid row, return False
          * @return False if the row accessed does not exist or does not contain any data
          */
-        return true;
-    }
 
 }
