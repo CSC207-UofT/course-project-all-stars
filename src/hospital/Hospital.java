9715826b6b6b6407c639f23c30b3b9e8964cc81c
euanpound <euanpound@gmail.com>
2021-12-04 15:03:09 -0500

    Commented Hospital.java

--- src/hospital/Hospital.java
+++ src/hospital/Hospital.java
@@ -28,11 +28,13 @@ public class Hospital {
     }
 
     public String admitPatient(Patient p) {
+        //Add a new patient and return a message
         patientsList.add(p);
         return "A new patient named " + p.getName() + " has been admitted!";
     }
 
     public Patient dischargePatient(int patientID) {
+        //Remove a patient given a patientID and return the patient object or a null if the patientID was not found
         for(Patient p: patientsList){
             if(p.getId() == patientID){
                 patientsList.remove(p);
@@ -43,11 +45,13 @@ public class Hospital {
     }
 
     public String hireDoctor(Doctor d) {
+        //Add a new doctor to the doctorslist
         doctorsList.add(d);
         return "A new doctor named " + d.getName() + " has been hired";
     }
 
     public String getHospitalDetails() {
+        //Return a short list of facts about the hospital
         return "This hospital," + " " +  this.name + " " + "founded in" + " " + this.foundedWhen + " " +
                 "is currently treating" + " " + this.patientsList.size() + " " + "patients.";
     }
