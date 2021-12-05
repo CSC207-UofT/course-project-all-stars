423f8df36f41455fe7ec58cfb3a8d97ffe256305
Ratantej Madan <70780442+ratantejmadan@users.noreply.github.com>
2021-12-04 22:43:06 -0500

    dec4 fixed errors to complete pull request

---
+++ src/cli/HospitalDetails.java
@@ -0,0 +1,27 @@
+package cli;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class HospitalDetails {
+
+//    public static void main(String[] args) throws SQLException, InvocationTargetException, IllegalAccessException {
+//        home();
+//    }
+
+    public static void hospitalDetails() throws SQLException, InvocationTargetException, IllegalAccessException {
+        System.out.println("Coming Soon in Phase 2");
+        System.out.println("1. Go Back");
+        int choice;
+        while (true) {
+            Scanner option = new Scanner(System.in);
+            System.out.println("Select your option:");
+            choice = option.nextInt();
+            if ((choice != 1)) {
+                System.out.println("Invalid Input!");
+            } else
+                break;
+        }
+    }
+}
