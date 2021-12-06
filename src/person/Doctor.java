f1ac3cce9cd546305a56922206c8fd03482d6f76
euanpound <euanpound@gmail.com>
2021-12-06 01:36:41 -0500

    Documented Doctor.java and removed unused and unecessary functions

--- src/person/Doctor.java
+++ src/person/Doctor.java
@@ -7,24 +7,32 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-    /*
-        Basic type of Doctor, without the specialization variable.
-    */
-
+/**
+ * A subclass of the Person Class. Doctors have a maximum of 25 patients that are stored in an ArrayList.
+ * Doctors can treat patients and have an ArrayList of curable diseases.
+ * Doctors are in the Entity Layer
+ *
+ * @version 2.0
+ * @since 1.0
+ */
 public class Doctor extends Person {
+    //Curable Diseases
     ArrayList<String> can_cure = new ArrayList<>(Arrays.asList("Salmonella", "Anaphylaxis",
             "Alcohol poisoning", "Shingles", "Chickenpox", "Seasonal Flu",
             "Tuberculosis", "Conjunctivitis", "Mononucleosis", "Strep Throat", "Headaches",
             "Lyme disease","Measles","Tetanus","Obesity", "Rabies", "Gastritis", "Anemia",
             "Hemorrhoids", "Decubitus Ulcer", "Celiac Disease"));
 
+    //Patients information
     final int max_patients = 25;
     ArrayList<Patient> patients;
 
+    //Constructor
     public Doctor(int id, String name, String address, String sex, int age) {
         super(id, name, address, sex, age);
     }
 
+    //Treat a patient that has a disease curable by the doctor and that is admitted
     public boolean treatPatient(Patient p) {
         if (p.getIs_Admitted()) {
             String disease = p.getDiagnose();
@@ -38,20 +46,22 @@ public class Doctor extends Person {
         return false;
     }
 
-    public void LearnHowToCure(String disease) {
-        if (!can_cure.contains(disease)) {
-            can_cure.add(disease);
-        }
+    //Returns a list of patients assigned to this doctor
+    public ArrayList<Patient> getPatients(){
+        return patients;
     }
 
+    //Returns a boolean that is true when a doctor has as many or more patients than they are allowed to have
     public boolean isFull(){
         return patients.size() >= max_patients;
     }
 
+    //Return an ArrayList of strings describing diseases they are capable of curing
     public ArrayList<String> getCurables(){
         return can_cure;
     }
 
+    //Assign a patient to this doctor
     public void assignPatient(Patient p){
         //Assigns Patient p to the doctor's patient list
         patients.add(p);
